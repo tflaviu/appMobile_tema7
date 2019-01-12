1fc3514c180168c75e1f1f6b2fddc6fc209f67fa
tflaviu <turcut.flv@gmail.com>
2019-01-13 00:07:57 +0200

    tema 8

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # appMobile_tema7
+
+Am folosit ionic 3, deoarece in 4 imi dadea eroare cand incercam sa folosesc toast-uri.
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,50 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  options = ['cash', 'creditCard', 'check', 'writeOff'];
+  optionsDetails = {
+    'cash' : {
+      name: 'Cash',
+      color: 'secondary',
+      icon: 'logo-usd',
+    },
+    'creditCard': {
+      name: 'Credit Card',
+      color: 'danger',
+      icon: 'card',
+    },
+    'check': {
+      name: 'Check',
+      color: 'light',
+      icon: 'logo-foursquare',
+    },
+    'writeOff': {
+      name: 'Write-Off',
+      color: 'dark',
+      icon: 'clipboard',
+    }
+  };
+  toastPositions = ['bottom', 'top', 'middle'];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController) {
 
+  }
+
+  showToast(message) {
+    // console.log(this.optionsDetails['cash'].color);
+    const toast = this.toastCtrl.create({
+      message: message,
+      position: this.toastPositions[Math.floor(Math.random()*this.toastPositions.length)],
+      duration: 3000
+    });
+
+    toast.present();
   }
 
 }
